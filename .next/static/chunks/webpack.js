340c3a022efd95ea2f945dd8ff70b30a14930c02
drnoir <chrispgodber@gmail.com>
2023-08-08 14:08:07 +0100

    # attempt push from laptop - push ui changes - working on menu - working on routes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9d6624412c8d9c6d"; }
+/******/ 		__webpack_require__.h = function() { return "aff0a5ecc9f79e13"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/components/MainMenu.tsx
+++ pages/components/MainMenu.tsx
@@ -1,13 +1,15 @@
-  <MainMenu />// MainMenu
+import Link from "next/link";
+
+<MainMenu />// MainMenu
 
 export default function MainMenu () {
-    return <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+    return <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start mt-2">
     <div className="flex flex-shrink-0 items-center">
       <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
     </div>
     <div className="hidden sm:ml-6 sm:block">
       <div className="flex space-x-4">
-        <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Take our Quiz</a>
+          <Link  className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page"href="/pages/Quiz" as="/quiz">Take Quiz</Link>
         <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Results</a>
         <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Charts</a>
         <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Share</a>
